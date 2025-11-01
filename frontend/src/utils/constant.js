3b66986ef2c3a8d39c423798d84abe21dc31c0a5
anozeref <kantorposfikry@gmail.com>
2025-11-01 16:22:59 +0700

    reset

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,13 +1,4 @@
 // src/constant.js
-export const API_BASE = import.meta.env.VITE_API_BASE || '/api';
-export const UPLOAD_ENDPOINT = `${API_BASE}/upload`;
-export const USERS_ENDPOINT = `${API_BASE}/users`;
-export const PROPERTIES_ENDPOINT = `${API_BASE}/properties`;
-export const NOTIFICATIONS_ENDPOINT = `${API_BASE}/notifications`;
-export const BANNED_USERS_ENDPOINT = `${API_BASE}/banned-users`;
-export const MEDIA_CLEANUP_ENDPOINT = `${API_BASE}/media/cleanup`;
-export const ADMIN_PROPERTY_STATUS_ENDPOINT = `${API_BASE}/admin/property-status`;
-
-// Updated to use API_BASE instead of localhost
-export const SOCKET_URL = `${API_BASE}`;
-export const MEDIA_URL = `${API_BASE}/media/`;
+export const API_URL = "http://localhost:3004/";
+export const SOCKET_URL = "http://localhost:3005";
+export const MEDIA_URL = "http://localhost:3005/media/";
